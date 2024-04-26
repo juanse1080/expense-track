df47ade8795ff965e6fbd2f4b6474037d5179b6d
Juan Marcon <juanmarcon1080@gmail.com>
2024-04-26 17:54:07 -0500

    feat(mobile): create a register view

---
+++ apps/mobile/src/schemas/register.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+export const registerSchema = z
+  .object({
+    name: z.string().min(1),
+    email: z.string().min(1).email(),
+    password: z.string().min(6),
+    confirmPassword: z.string().min(6),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  })
+export type registerSchemaType = z.infer<typeof registerSchema>
